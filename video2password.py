ca408971d9a5b415d82b357555574150d88d4b6c
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-04-12 13:58:51 -0700

    Update video2password.py

--- video2password.py
+++ video2password.py
@@ -1,25 +1,48 @@
-''' Video to Password
-Maybe what they say is what they mean?  Let's see?  Add a video clip for your
-data and see if your output unlocks any doors.  Good luck!
-
+'''Video to Password
+Maybe what they say is what they mean?  Let's see.  Add a video clip for your
+data and see if it unlocks any doors.  Good luck!
 
 Details: Import moviepy and SpeechRecognition to pull that data.  Translate the
-video into audio files, then translate the audio into text, then output to custom file.
+video into audio files, then translate the audio into text, then output the
+results to a custom file.
+
+    Acceptable Video Formats:
+        MP4 (mp4, m4a, m4v, f4v, f4a, m4b, m4r, f4b, mov)
+        3GP (3gp, 3gp2, 3g2, 3gpp, 3gpp2)
+        OGG (ogg, oga, ogv, ogx)
+        WMV (wmv, wma, asf*)
+    Audio Formats:
+        MP3
+        AAC
+        WMA
+        AC3 (Dolby Digital)
+
 Author: Nick Sepe
 
 '''
-
 import speech_recognition as sr
 import moviepy.editor as mp
+import os
 
-# create a function to read the file and make sure it's a valid format
+# function to read the file
+# (TO DO:make sure it's a valid format)
 def theFile():
+    # your_file will be what we pass to our main function
+    global your_file
     your_file = input("Copy paste your video file:")
-    fileLength = []
-    for i in your_file:
-        fileLength.append(i)
-    n = len(fileLength)
-    print(fileLength[n-4:n])
+    # below is for valid format checks, TODO!
+    # filesplit = os.path.splitext(your_file)
+    # fileIs = filesplit[1]
+
 theFile()
-#clip = mp.VideoFileClip(your_file)
-#clip.audio.write_audiofile(r"converted.wav")
+# if you need to test global results un-comment the print()s below
+# print(your_file)
+
+# function to convert your_file into .wav format
+def convertCLip():
+    nameTxt = input("What do you want to name the text file? (Do not add the .txt):")
+    # your_file  = str(your_file)
+    clip = mp.VideoFileClip(your_file)
+    clip.audio.write_audiofile(nameTxt+".wav")
+
+convertCLip()
