0b8d546d25ca328e4c30c4422ab411bb90269da4
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-09-04 19:13:08 -0700

    Update video2password.py

--- video2password.py
+++ video2password.py
@@ -1,4 +1,4 @@
-'''                  Video to Password
+'''                  Video 2 Password
 
 Maybe what they say is what they mean?  Let's see.  Add a video clip for your
 data and see if it unlocks any doors.  Good luck!
@@ -9,9 +9,29 @@ data and see if it unlocks any doors.  Good luck!
         OGG (ogg, oga, ogv, ogx)
         WMV (wmv, wma, asf*)
 
-Author: Blenno
 '''
-import convertClip as cc
+
+import moviepy.editor as mp
+
+def convert_to_audio(file):
+
+    clip = mp.VideoFileClip(file)
+    clip.audio.write_audiofile(nameAudio+".wav")
+    new_audio = (nameAudio+".wav")
+    print("Your new file" + new_audio + "is stored in the same folder as this script now.")
+    return new_audio
+
 # make sure the file you pass is in the same directory
-data = input("What is the name of your file? (include extension):")
-cc.convert(data)
+
+def get_file():
+    print("###############################################################################")
+    print("# Convert a video to a password file.                                         #")
+    print("# Make sure the file you want to convert is in the same folder as this script.#")
+    print("###############################################################################")
+    nameAudio = input("What do you want to name the audio file? (Do not add extension):  ")
+    data = input("What is the name of your file? (include extension): ")
+    return data
+
+if __name__ == '__main__':
+    data = get_file()
+    convert_to_audio(data)
