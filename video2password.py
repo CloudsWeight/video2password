1145a2c8c2e7865a1753013fa8ab1fc11a17d97a
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-09-04 19:32:26 -0700

    Update video2password.py

--- video2password.py
+++ video2password.py
@@ -1,7 +1,6 @@
 '''                  Video 2 Password
 
-Maybe what they say is what they mean?  Let's see.  Add a video clip for your
-data and see if it unlocks any doors.  Good luck!
+Currently only converts video to .wav audio.  
 
     Acceptable Video Formats:
         MP4 (mp4, m4a, m4v, f4v, f4a, m4b, m4r, f4b, mov)
@@ -9,29 +8,40 @@ data and see if it unlocks any doors.  Good luck!
         OGG (ogg, oga, ogv, ogx)
         WMV (wmv, wma, asf*)
 
+Author: CloudsWeight
 '''
 
 import moviepy.editor as mp
 
-def convert_to_audio(file):
+def banner():
+    print("\n###############################################################################")
+    print("# Convert a video to a password file.                                         #")
+    print("# Make sure the file you want to convert is in the same folder as this script.#")
+    print("###############################################################################")
 
+
+def name_file():
+    nameAudio = input("What do you want to name the audio file? (Do not add extension):  ")
+    return nameAudio
+
+def get_file():
+    data = input("What is the name of your file? (include extension): ")
+    return data
+
+def convert_to_audio(file, nameAudio):
     clip = mp.VideoFileClip(file)
     clip.audio.write_audiofile(nameAudio+".wav")
     new_audio = (nameAudio+".wav")
-    print("Your new file" + new_audio + "is stored in the same folder as this script now.")
+    print("Your new file \"" + new_audio + "\" is stored in the same folder as this script now.")
     return new_audio
 
 # make sure the file you pass is in the same directory
 
-def get_file():
-    print("###############################################################################")
-    print("# Convert a video to a password file.                                         #")
-    print("# Make sure the file you want to convert is in the same folder as this script.#")
-    print("###############################################################################")
-    nameAudio = input("What do you want to name the audio file? (Do not add extension):  ")
-    data = input("What is the name of your file? (include extension): ")
-    return data
+
 
 if __name__ == '__main__':
+    banner()
     data = get_file()
-    convert_to_audio(data)
+    name = name_file()
+    convert_to_audio(data, name)
+
