5871e05ce65c94d5166f98e9d193cdd042c84672
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-09-04 19:14:04 -0700

    Delete convertClip.py

--- convertClip.py
+++
@@ -1,8 +0,0 @@
-''' module to convert video audio to a wav file, read moviepy docs '''
-import moviepy.editor as mp
-def convert(file):
-    global new_audio
-    nameAudio = input("What do you want to name the audio file? (Do not add extension):")
-    clip = mp.VideoFileClip(file)
-    clip.audio.write_audiofile(nameAudio+".wav")
-    new_audio = (nameAudio+".wav")
